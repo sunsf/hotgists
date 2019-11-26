9dc003565ba4b91f3d2575d52bd2790c8d728ef9
william <william.sun@hkactech.com>
2019-11-27 04:51:19 +0800

    Initial Commit

---
+++ HotGists/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  HotGists
+//
+//  Created by William SUN on 27/11/2019.
+//  Copyright © 2019 William SUN. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
